1cf81aa1c25a2190e5be8c508c280fcea021e121
gillesarnaudasse65-web <gillesarnaudasse65@gmail.com>
2026-07-27 17:53:23 +0000

    Ajoute app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.vitalis.healthos"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.vitalis.healthos"
+        minSdk = 28
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.activity:activity-ktx:1.10.0")
+    implementation("androidx.health.connect:connect-client:1.1.0")
+    implementation("androidx.webkit:webkit:1.12.1")
+}
